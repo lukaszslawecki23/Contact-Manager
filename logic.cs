a5fa2172ee857180cf7075bf754bce2ce7ef245d
lukaszslawecki23 <lukasz.slawecki.23@gmail.com>
2026-05-11 00:31:28 +0200

    Menu update

--- logic.cs
+++ logic.cs
@@ -1,11 +1,12 @@
-using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 
 namespace contact_manager
 {
     class Logic
     {
         private List<Contact> _contacts = new List<Contact>();
+        private string _fileName = "contact_data.json";
         
         public void AddContact()
         {
@@ -15,7 +16,7 @@ namespace contact_manager
             newContact.FirstName = Console.ReadLine();
             Console.Write("Last name: ");
             newContact.LastName = Console.ReadLine();
-            Console.Write("Phone numbera: ");
+            Console.Write("Phone number: ");
             newContact.PhoneNumber = Console.ReadLine();
             Console.Write("Email address: ");
             newContact.EmailAddress = Console.ReadLine();
@@ -32,7 +33,8 @@ namespace contact_manager
             if (isValid)
             {
                 _contacts.Add(newContact);
-                Console.WriteLine($"{newContact.FirstName}, {newContact.LastName}, {newContact.PhoneNumber}, {newContact.EmailAddress}, {newContact.HomeAddress}, {newContact.OptionalNote}");
+                AddToFile();
+                Console.WriteLine($"{newContact.FirstName}, {newContact.LastName}, {newContact.PhoneNumber}");
             }
             else
             {
@@ -41,6 +43,42 @@ namespace contact_manager
                 {
                     Console.WriteLine($"- {error.ErrorMessage}");
                 }
+            }
+        }
+
+        public void ShowContacts()
+        {
+            if (_contacts.Count == 0)
+            {
+                Console.WriteLine("Contact list empty");
+                return;
+            }
+            
+            Console.WriteLine("Your contacts:");
+
+            for (int i = 0; i < _contacts.Count; i++)
+            {
+                Contact currentContact = _contacts[i];
+                
+                Console.WriteLine(currentContact);
+            }
+        }
+
+        private void AddToFile()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string convertedString = JsonSerializer.Serialize(_contacts, options);
+            File.WriteAllText(_fileName, convertedString);
+            
+            Console.WriteLine($"Data added to file {_fileName}");
+        }
+
+        public void LoadFromFile()
+        {
+            if (File.Exists(_fileName))
+            {
+                string jsonString = File.ReadAllText(_fileName);
+                _contacts = JsonSerializer.Deserialize<List<Contact>>(jsonString);
             }
         }
     }
